f62d567d6b2bd7bdec6638b5822663b030454e8c
Andrej <andrejw39@gmail.com>
2022-07-19 23:02:03 +0200

    Settings: Revamp WannabeOS UI v1.0 This is not the final change. Changes will be made later on. Changes inspired by https://github.com/syberia-project/platform_packages_apps_Settings/commit/9cbefb18bb5c4045446a5e12ef44b9490d41e965.

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -324,4 +324,9 @@ public class Settings extends SettingsActivity {
      * Activity for AppDashboard.
      */
     public static class AppDashboardActivity extends SettingsActivity {}
+
+    /**
+     * Wannabe Customisation Activity
+     */
+    public static class SomethingSettingsActivity extends SettingsActivity {}
 }
